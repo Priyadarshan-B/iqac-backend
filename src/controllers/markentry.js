ce6f1998cc33556c01872998982918d08bd3c6a4
Sabareesh22 <sabareesh.cs22@bitsathy.ac.in>
2024-06-05 09:47:17 +0530

    Marks retrieving complete

--- src/controllers/markentry.js
+++ src/controllers/markentry.js
@@ -16,9 +16,11 @@ exports.updateMarks = (req, res) => {
   
       student.marks.forEach((mark, i) => {
         const co = req.body.co[i].id;
-  
-        db.query(
-          `INSERT INTO mark_entry(student,type,co_id,mark) VALUES (?,?,?,?)`,
+        const query = `
+        REPLACE INTO mark_entry (student, type, co_id, mark)
+VALUES (?, ?, ?, ?);
+        `
+        db.query(query,
           [id, type, co, mark],
           (err) => {
             if (err) {
@@ -36,3 +38,22 @@ exports.updateMarks = (req, res) => {
   
     return res.status(200).send("Mark Entered");
   };
+
+  exports.getMarks = (req,res)=>{
+    const query = `SELECT student,type,co_id,mark,present FROM mark_entry WHERE co_id = ? AND type = ?`
+    db.query(query,[req.query.co_id,req.query.type],(err,rows)=>{
+          if(err){
+            console.log(err)
+          }
+          else{
+            let mark_dict = {}
+            rows.forEach(element => {
+              mark_dict['S'+element.student+'C'+element.co_id+'T'+element.type] = {mark:element.mark,present:element.present}
+            });
+
+            res.send(mark_dict)
+
+            console.log(mark_dict)
+          }
+    })
+  }
--- src/routes/markentry_route.js
+++ src/routes/markentry_route.js
@@ -3,5 +3,5 @@ const markentryController = require('../controllers/markentry')
 const router  = express.Router();
 
 router.post('/updateMarks',markentryController.updateMarks)
-
+router.get('/marks',markentryController.getMarks)
 module.exports = router
